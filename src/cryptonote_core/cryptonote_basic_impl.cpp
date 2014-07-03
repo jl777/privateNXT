903795971e3522b0124d7fd98041016f07b892dd
jl777 <jl777@users.noreply.github.com>
2014-07-03 00:58:57 -0500

    Update cryptonote_basic_impl.cpp

--- src/cryptonote_core/cryptonote_basic_impl.cpp
+++ src/cryptonote_core/cryptonote_basic_impl.cpp
@@ -35,7 +35,7 @@ namespace cryptonote {
   //-----------------------------------------------------------------------------------------------
   bool get_block_reward(size_t median_size, size_t current_block_size, uint64_t already_generated_coins, uint64_t &reward) {
     uint64_t base_reward = (MONEY_SUPPLY - already_generated_coins) >> EMISSION_SPEED_FACTOR;
-reward = 1000;
+reward = 1000 * 100000000L;
 return(true);
     //make it soft
     if (median_size < CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE) {
