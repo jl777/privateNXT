ea9867347ff6d40bb98903f6669186990f6435d0
jl777 <jl777@users.noreply.github.com>
2014-07-03 03:25:38 -0500

    Update cryptonote_format_utils.cpp

--- src/cryptonote_core/cryptonote_format_utils.cpp
+++ src/cryptonote_core/cryptonote_format_utils.cpp
@@ -634,7 +634,7 @@ namespace cryptonote
 
     //hard code coinbase tx in genesis block, because "true" generating tx use random, but genesis should be always the same
     //TODO After you obtain hash of the genesis block put it here and recompile sources!
-    std::string genesis_coinbase_tx_hex = "010d01ff000180d0dbc3f402029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd088071210108d0ce9e890138acc49e6b868654d5fa4ad88ddb77dc928ff9b7ea3fac95f294";
+    std::string genesis_coinbase_tx_hex = "010d01ff000180d0dbc3f402029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd0880712101706c86a9a3bc667ea20f7c67b5f4e1366ea47693cf47f595ff0f4b1c858a026a";
 
     blobdata tx_bl;
     string_tools::parse_hexstr_to_binbuff(genesis_coinbase_tx_hex, tx_bl);
